55907ce1b0adf92f4e8c8fde4f6700b331a6be6b
Levi Horst <levi.horst@bruins.belmont.edu>
2023-10-31 12:10:14 -0500

    now uses a cursor to show the list of pokemon

--- app/src/main/java/com/example/pokedex/ListOfEntries.java
+++ app/src/main/java/com/example/pokedex/ListOfEntries.java
@@ -16,6 +16,7 @@ import java.util.LinkedList;
 public class ListOfEntries extends AppCompatActivity {
 
     ArrayList<String> pokelist;
+    LinkedList<Pokemon> pokemon;
     ListView pokelistLV;
     Cursor mCursor;
     public ListOfEntries(){}
@@ -32,7 +33,36 @@ public class ListOfEntries extends AppCompatActivity {
     }
 
     public void makeList(){
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pokelist);
+
+        mCursor = getContentResolver().query(
+                PokeProvider.contentURI, null, null,
+                null, null);
+        pokemon = new LinkedList<>();
+        if(mCursor != null){
+            mCursor.moveToFirst();
+            if(mCursor.getCount() > 0){
+                while(mCursor.isAfterLast() == false){
+                    int a = mCursor.getInt(1);
+                    String b = mCursor.getString(2);
+                    String c = mCursor.getString(3);
+                    String d = mCursor.getString(4);
+                    float e = mCursor.getFloat(5);
+                    float f = mCursor.getFloat(6);
+                    int g = mCursor.getInt(7);
+                    int h = mCursor.getInt(8);
+                    int i = mCursor.getInt(9);
+                    int j = mCursor.getInt(10);
+
+                    pokemon.add(new Pokemon(a, b, c, d, e , f, g , h , i , j));
+                    mCursor.moveToNext();
+                }
+            }
+        }
+
+        ArrayAdapter<Pokemon> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pokemon);
         pokelistLV.setAdapter(adapter);
+
+        // ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pokelist);
+        // pokelistLV.setAdapter(adapter);
     }
 }
